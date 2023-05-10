47f26aa8dc7905ee7cfb210eae78905b130325dd
=muskan-batra <68785899+muskan-batra@users.noreply.github.com>
2023-05-10 12:47:19 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Header.css'
+function Header() {
+  return (
+    <nav> 
+      <a href='/Home'>Home</a>
+      <a href='About'>About</a>
+      <a href='/Contact'>Contact</a>
+    </nav>
+  )
+}
+
+export default Header
